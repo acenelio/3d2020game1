d20e95d64d7fe7329e243f2ac9927f6d58592f41
Nelio Alves <acenelio@gmail.com>
2019-12-16 00:40:23 -0300

    PlayerMove

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    float Speed = 4f;
+
+    Rigidbody Rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Rb.velocity = new Vector3(h * Speed, Rb.velocity.y, v * Speed);
+    }
+}
